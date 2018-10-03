641225a3d9176cd76f242a746db56397f81888cd
Jim Mullen <mullen2@iu.edu>
2018-10-03 09:07:57 -0400

    Added dev dependencies and first phpunit test

---
+++ tests/unit/DbHostsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace IU\RedCapEtlModule;
+
+use PHPUnit\Framework\TestCase;
+
+class DbHostsTest extends TestCase {
+    public function setup()
+    {
+    }
+
+    public function testCreate()
+    {
+        $dbHosts = new DbHosts();
+        $this->assertNotNull($dbHosts);
+    }
+}
