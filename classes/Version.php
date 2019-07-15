9c061ee6fafc8fe48a6abf13d698ee552933bee2
Jim Mullen <mullen2@iu.edu>
2019-07-15 09:25:14 -0400

    Added version file

---
+++ classes/Version.php
@@ -0,0 +1,16 @@
+<?php
+#-------------------------------------------------------
+# Copyright (C) 2019 The Trustees of Indiana University
+# SPDX-License-Identifier: BSD-3-Clause
+#-------------------------------------------------------
+
+namespace IU\RedCapEtlModule;
+
+/**
+ * Class for storing software version information.
+ */
+class Version
+{
+    # This release number should be updated each time a release is made.
+    const RELEASE_NUMBER = "0.0.4";
+}
