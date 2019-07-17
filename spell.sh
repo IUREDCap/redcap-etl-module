8040f51b76051be93303de33ed6016460d5b96cd
Jim Mullen <mullen2@iu.edu>
2019-07-17 15:06:28 -0400

    Added initial spell check script

---
+++ spell.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#-------------------------------------------------------
+# Copyright (C) 2019 The Trustees of Indiana University
+# SPDX-License-Identifier: BSD-3-Clause
+#-------------------------------------------------------
+
+files=( "./web/user_manual.php" "./web/admin/info.php" );
+
+for file in "${files[@]}"
+do
+    echo "FILE: ${file}";
+    echo "===============================================================";
+    cat ${file} \
+        | sed 's/[aA]dmin//g;' \
+        | sed 's/API//g;' \
+        | sed 's/ETL//g;' \
+        | sed 's/github//g;' \
+        | sed 's/REDCap-ETL//g;' \
+        | sed 's/REDCap//g;' \
+        | sed 's/<[^>]*>//g;' \
+        | sed 's/use [_a-zA-Z0-9\\]*;//g' \
+        | sed 's/require_once[^;]*;//g' \
+        | sed 's/\@[_a-zA-Z][_a-zA-Z0-9]*//g' \
+        | sed 's/\$[_a-zA-Z][_a-zA-Z0-9]*//g' \
+        | sed 's/->[_a-zA-Z][_a-zA-Z0-9]*(//g' \
+        | sed 's/[_a-zA-Z][_a-zA-Z0-9]*(//g' \
+        | sed 's/[_a-zA-Z][_a-zA-Z0-9]*::[_a-zA-Z][_a-zA-Z0-9]*//g' \
+        | sed 's/\?[_a-zA-Z][_a-zA-Z0-9]*=//g' \
+        | sed 's/\&[_a-zA-Z][_a-zA-Z0-9]*=//g' \
+        | sed 's/RedCapEtlModule//g;' \
+        | spell \
+        | sort \
+        | uniq;
+    echo;
+done
+
