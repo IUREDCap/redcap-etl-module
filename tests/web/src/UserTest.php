7ce5f8593c1a94c36dc3d34b39ba54fd876a99d5
Jim Mullen <mullen2@iu.edu>
2020-02-21 13:23:00 -0500

    Added a file copyright notice

--- tests/web/src/UserTest.php
+++ tests/web/src/UserTest.php
@@ -1,4 +1,8 @@
 <?php
+#-------------------------------------------------------
+# Copyright (C) 2020 The Trustees of Indiana University
+# SPDX-License-Identifier: BSD-3-Clause
+#-------------------------------------------------------
 
 namespace IU\RedCapEtlModule\WebTests;
 
