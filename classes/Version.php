15e6321764dc0e48f399d8e8753bc524c493d8eb
Jim Mullen <mullen2@iu.edu>
2020-04-14 16:38:19 -0400

    Updated version release number to 1.2.1

--- classes/Version.php
+++ classes/Version.php
@@ -12,5 +12,5 @@ namespace IU\RedCapEtlModule;
 class Version
 {
     # This release number should be updated each time a release is made.
-    const RELEASE_NUMBER = "1.2.0";
+    const RELEASE_NUMBER = "1.2.1";
 }
